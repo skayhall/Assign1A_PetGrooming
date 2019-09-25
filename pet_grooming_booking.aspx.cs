6982f41207e06b26c6392d588ee317f040a7ffb9
Shanakay <shanakayhall@gmail.com>
2019-09-24 20:17:04 -0400

    commit-cb

--- pet_grooming_booking.aspx.cs
+++ pet_grooming_booking.aspx.cs
@@ -12,6 +12,62 @@ namespace N01119610_Assignment1_A
         protected void Page_Load(object sender, EventArgs e)
         {
 
+
+            if (Page.IsPostBack)
+            {
+                Page.Validate();
+
+                if (Page.IsValid)
+                {
+
+
+
+                    
+                    string Pet_OwnerName = petowner_name.Text.ToString();
+                    string Pet_Name = pet_name.Text.ToString();
+                    string Grooming_DayofWeek = grooming_dayofweek.SelectedValue.ToString();
+                    string Animal_Tyle = animal_type.SelectedValue.ToString();
+                    int Guest_Friendcount = Convert.ToInt32(guest_friendcount.Text);
+                    string Pet_OwnerPhone = petowner_phone.Text.ToString();
+
+                    
+                    confirmbox.InnerHtml = "Thank you for RSVPing! Your Summary is as follows: <br>";
+
+                    confirmbox.InnerHtml += " First Name: " + Guest_Fname + "<br>";
+                    confirmbox.InnerHtml += " Last Name: " + Guest_Lname + "<br>";
+                    confirmbox.InnerHtml += " Costume Type: " + Guest_Costumetype + "<br>";
+
+
+
+                    confirmbox.InnerHtml += " You are bringing " + Guest_Friendcount.ToString() + " friend(s) " + "<br>";
+                    if (Guest_Friendcount == 0)
+                    {
+                        confirmbox.InnerHtml += "You'll make friends at the party";
+                    }
+
+                    if (Guest_Friendcount == 1)
+                    {
+                        confirmbox.InnerHtml += "Glad you found a friend!";
+                    }
+
+                    if (Guest_Friendcount == 2)
+                    {
+                        confirmbox.InnerHtml += "Three's a crowd, isn't it?";
+                    }
+
+
+
+                    confirmbox.InnerHtml += " We can reach you at: " + Guest_Phone;
+
+                }
+            }
+        }
+    }
+}
+
+
+
+
         }
     }
 }
